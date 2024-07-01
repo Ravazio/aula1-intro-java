23f271dbc6cc4c4c06251de45ca6aa7ae00e5fdb
Victor Ravazio de Lima <limarvict@hotmail.com>
2024-07-01 11:21:17 +0000

    Pending changes exported from your codespace

---
+++ src/Selecao.java
@@ -0,0 +1,45 @@
+public class Selecao {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if(a != b){
+            System.out.println("a != b");
+        }
+
+        if (a == b){
+            System.out.println("a == b");
+        }else{
+            System.out.println("a != b");
+        }
+
+        if (a == b){
+            System.out.println("a == b");
+        }else if (a < b){
+            System.out.println("a < b");
+        }else{
+            System.out.println("a > b");
+        }
+
+        String nomeDia = null;
+        int dia = 3;
+
+        switch (dia) {
+            case 1:
+                nomeDia = "Domingo";
+                break;
+            case 2:
+                nomeDia = "Segunda";
+                break;
+            case 3:
+                nomeDia = "Terça";
+                break;
+            default:
+                System.out.println("Dia invalido!");
+                break;
+        }
+
+        System.out.println(nomeDia.toUpperCase());
+        System.out.println(nomeDia);
+    }
+}
